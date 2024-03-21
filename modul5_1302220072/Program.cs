7b9eb0d4ddc05530d866969dbf7e5d1ef081911f
shuabruh <shuabruh@student.telkomuniversity.ac.id>
2024-03-21 10:41:14 +0700

    menambahkan class SimpleDataBase

--- modul5_1302220072/Program.cs
+++ modul5_1302220072/Program.cs
@@ -4,6 +4,10 @@
     {
         Penjumlahan objPenjumlahan = new Penjumlahan();
         Console.WriteLine(objPenjumlahan.jumlahTigaAngka<float>((float)1.2, (float)5.5, (float)1.4));
+
+        SimpleDataBase<float> db = new SimpleDataBase<float>();
+        db.addNewData((float)19.22);
+        db.printAllData();
     }
 
     public class Penjumlahan
@@ -11,6 +15,32 @@
         public T jumlahTigaAngka<T>(T a, T b, T c)
         {
             return (dynamic)a + (dynamic)b + (dynamic)c;
+        }
+    }
+}
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void addNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void printAllData()
+    {
+        for(int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i+1) + " berisi: " + storedData[i] + " yang disimpan pada waktu UTC: " + inputDates[i]);
         }
     }
 }
