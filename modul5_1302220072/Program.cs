083e8b05f883c09bb765cfe1fbcd789db514dbfe
shuabruh <shuabruh@student.telkomuniversity.ac.id>
2024-03-21 10:13:13 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- modul5_1302220072/Program.cs
+++ modul5_1302220072/Program.cs
@@ -2,6 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Penjumlahan objPenjumlahan = new Penjumlahan();
+        Console.WriteLine(objPenjumlahan.jumlahTigaAngka<float>((float)1.2, (float)5.5, (float)1.4));
+    }
+
+    public class Penjumlahan
+    {
+        public T jumlahTigaAngka<T>(T a, T b, T c)
+        {
+            return (dynamic)a + (dynamic)b + (dynamic)c;
+        }
     }
 }
